85c13fb94ce554c66a2172dcdfb7469b1cadde6c
sxi8022 <sxi8022@gmail.com>
2019-12-17 09:43:38 +0900

    라우터설치

--- invfrontend/src/pages/index.vue
+++ invfrontend/src/pages/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div id="index">
-    <Menu></Menu>
+  <div style="width:100%; height:100%; position:fixed" >
+    <div style="height:10%; width:100%">
+      <div>
+        {{title}}
+      </div>
+    </div>
+    <div style="height:90%; width:100%;">
+      <div style="height:100%;width:30%; float:left;">
+        <Menu></Menu>
+      </div>
+      <div style="height:100%; width:70%;">
+
+      </div>
+    </div>
   </div>
 </template>
 
--- invfrontend/src/router/index.js
+++ invfrontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/pages/index'
-
+import inventory from '@/pages/inv/inventory'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/inventory',
+      component: inventory
     }
   ]
 })
