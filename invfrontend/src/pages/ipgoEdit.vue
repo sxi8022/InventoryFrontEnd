666d6ff3e8faf6879d58ff6204701cf63f12793e
sxi8022 <sxi8022@gmail.com>
2019-12-19 16:52:10 +0900

    수정,등록페이지구분

--- invfrontend/src/pages/ipgoEdit.vue
+++ invfrontend/src/pages/ipgoEdit.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input id="parentData" type="hidden"/>
+        <input id="parentData" type="hidden" v-model="ipgoSpeData"/>
         <input id="stockNo" type="hidden"/>
         <div class="btn">
             <button>닫기</button>
@@ -33,8 +33,17 @@ export default {
       matNm: ''
     }
   },
+  props: ['ipgoSpeData'],
   async mounted () {
     await this.getMaterialData()
+
+    var parentData = $('#parentData').val().split(',')
+    $('#stockNo').val(parentData[0])
+    $('#ipchulDate').val(parentData[1])
+    $('#matNo').val(parentData[1])
+    $('#itemNo').val(parentData[4])
+    $('#ipchulCnt').val(parentData[5])
+    $('#rmk').val(parentData[6])
   },
   methods: {
     closeDialog () {
