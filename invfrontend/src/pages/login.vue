723d700da4b62d82a8cbbde818ab5ec03880be87
sxi8022 <sxi8022@gmail.com>
2019-12-30 17:54:33 +0900

    ddd

--- invfrontend/src/pages/login.vue
+++ invfrontend/src/pages/login.vue
@@ -9,17 +9,18 @@
             <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
             </div>
             <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-              <form>
+              <form v-on:submit.prevent="loginUser">
                 <div class="form-group">
                     <label for="userid">사용자ID</label>
-                    <input type="text" name="userid" class="form-control" />
+                    <input type="text" v-model="userid" name="userid" class="form-control" />
+                    <small>{{ idValidation }}</small>
                 </div>
                 <div class="form-group">
                     <label for="password">패스워드</label>
-                    <input type="password" name="password" class="form-control" />
+                    <input type="password" v-model="password" name="password" class="form-control" />
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-primary">로그인</button>
+                    <button type="submit" class="btn btn-primary">로그인</button>
                 </div>
               </form>
             </div>
@@ -34,7 +35,36 @@ export default {
   name: 'login',
   data () {
     return {
-      msg: '로그인'
+      msg: '로그인',
+      userid: '',
+      password: '',
+      loginData: {
+        userid: '',
+        password: '',
+        state: ''
+      }
+    }
+  },
+  computed: {
+    idValidation: function () {
+      return this.userid.length > 4 ? '' : '4자 이상 입력해주세요.'
+    }
+  },
+  methods: {
+    loginUser () {
+      var strTemp = 'http://10.10.11.33:80/api/Account'
+      this.loginData = {
+        userid: this.$refs.userid.value,
+        password: this.$refs.password.value,
+        state: ''
+      }
+      this.axios.post(strTemp, this.loginData).then(res => {
+        if (res.state === 'SUCCESS') {
+          alert('성공하였습니다.')
+        } else {
+          alert('실패하였습니다.')
+        }
+      })
     }
   }
 }
